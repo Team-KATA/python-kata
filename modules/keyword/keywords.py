9e3b8075d02c6f68c4c5388d614cb0852e685bf3
msjk27 <bosungsuk08@gmail.com>
2023-10-20 05:04:57 +0000

    keyword 수정

--- modules/keyword/keywords.py
+++ modules/keyword/keywords.py
@@ -4,10 +4,13 @@ from transformers import BertModel
 from collections import Counter
 from konlpy.tag import Okt
 
+model = BertModel.from_pretrained('skt/kobert-base-v1')
+kiwi = Kiwi()
+kw_model = KeyBERT(model)
+
 
 def __noun_extractor(text):
     results = []
-    kiwi = Kiwi()
     result = kiwi.analyze(text)
     for token, pos, _, _ in result[0][0]:
         if len(token) != 1 and pos.startswith('N') or pos.startswith('SL'):
@@ -16,10 +19,7 @@ def __noun_extractor(text):
 
 
 def keyword_extractor(article_text: str) -> list:
-  model = BertModel.from_pretrained('skt/kobert-base-v1')
-  kw_model = KeyBERT(model)
-  kiwi = Kiwi()
-  kiwi.analyze(article_text)
-  nouns = __noun_extractor(article_text)
-  text = ' '.join(nouns)
-  return kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words=None, top_n=30)
+    kiwi.analyze(article_text)
+    nouns = __noun_extractor(article_text)
+    text = ' '.join(nouns)
+    return kw_model.extract_keywords(text, keyphrase_ngram_range=(1, 1), stop_words=None, top_n=30)
