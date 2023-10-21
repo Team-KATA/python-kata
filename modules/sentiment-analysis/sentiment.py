a00a3768eb2cbe92912d2dd116b5c0e154e5ad21
msjk27 <bosungsuk08@gmail.com>
2023-10-21 07:26:27 +0000

    Feat: optimized sentiment.py

--- modules/sentiment-analysis/sentiment.py
+++ modules/sentiment-analysis/sentiment.py
@@ -1,8 +1,8 @@
 from transformers import pipeline
+pipe_translation_ko_en = pipeline("translation", model="circulus/kobart-trans-ko-en-v2")
+pipe_senti = pipeline("sentiment-analysis")
 
 def sentiment_analysing(text):
-  pipe_translation_ko_en = pipeline("translation", model="circulus/kobart-trans-ko-en-v2")
   dic = pipe_translation_ko_en(text ,max_length=1000)
   translated = dic[0]["translation_text"]
-  pipe_senti = pipeline("sentiment-analysis")
   return pipe_senti(translated)
