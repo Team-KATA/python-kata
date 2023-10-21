4ee01e827b304676f97d61ae926bc13e45e17131
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2023-10-21 18:02:20 +0900

    Feat prediction (#10)
    * init
    
    * Feat: create prediction module
    
    * Feat: modify function's
    
    * Feat: change prediction function's parameter

---
+++ modules/prediction/prediction.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+# from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from pmdarima import auto_arima
+
+# 상대경로
+def load_csv(_file_name: str, _index_col: str):
+    df = pd.read_csv(_file_name)
+    df.set_index(_index_col)
+    return df
+
+def defferencing_data(_df, _col: str):
+    diff_df = _df.copy()
+    diff_df = diff_df[_col].diff().dropna()
+    return diff_df
+
+def optimize_order(_df):
+    return auto_arima(_df,  seasonal=False, m=12, trace=True, suppress_warnings=True)
+
+def train(_df, _col, _order):
+    # ARIMA 모델 정의 및 학습
+    model = SARIMAX(_df[_col], order=_order.order, seasonal_order=_order.seasonal_order)
+    # 모델 훈련
+    model_fit = model.fit()
+    return model_fit
+
+def forecast(_model: SARIMAX, _start: int, _end: int) -> pd.core.series.Series:
+    _forecast = _model.predict(start=_start, end=_end)
+    _forecast.plot()
+    return _forecast
+
+def prediction(_df: str, _col: str, _index_col: str, _amount: float) -> list:
+    df = load_csv(_df, _index_col)
+    diff_df = defferencing_data(df, _col)
+    best_order = optimize_order(diff_df)
+    model = train(df, _col, best_order)
+    result = forecast(model, int(len(df)*(1-_amount)), len(df)-1)
+    return list(result)
+
+
+result = prediction("arima_train - full.csv", "dew", "datetime", 0.02)
+print(f'result : \n{result}')
+    
