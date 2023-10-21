8c56f8ac5e778bdae37dfe5b5aa61c6b4a0fa597
POBSIZ <gjehdtjr911@gmail.com>
2023-10-21 08:49:39 +0000

    Feat: modify function's

--- modules/prediction/prediction.py
+++ modules/prediction/prediction.py
@@ -5,12 +5,12 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from pmdarima import auto_arima
 
 # 상대경로
-def load_csv(_file_name, _index_col):
+def load_csv(_file_name: str, _index_col: str):
     df = pd.read_csv(_file_name)
     df.set_index(_index_col)
     return df
 
-def defferencing_data(_df, _col):
+def defferencing_data(_df, _col: str):
     diff_df = _df.copy()
     diff_df = diff_df[_col].diff().dropna()
     return diff_df
@@ -25,12 +25,12 @@ def train(_df, _col, _order):
     model_fit = model.fit()
     return model_fit
 
-def forecast(_model, _start, _end):
+def forecast(_model: SARIMAX, _start: int, _end: int) -> pd.core.series.Series:
     _forecast = _model.predict(start=_start, end=_end)
     _forecast.plot()
     return _forecast
 
-def prediction(_df, _col, _index_col, _start):
+def prediction(_df: str, _col: str, _index_col: str, _start: float) -> list:
     df = load_csv(_df, _index_col)
     diff_df = defferencing_data(df, _col)
     best_order = optimize_order(diff_df)
@@ -39,6 +39,6 @@ def prediction(_df, _col, _index_col, _start):
     return list(result)
 
 
-result = prediction("arima_train - full.csv", "dew", "datetime", 0.98)
-print(f'result : \n{result}')
+# result = prediction("arima_train - full.csv", "dew", "datetime", 0.98)
+# print(f'result : \n{result}')
     
