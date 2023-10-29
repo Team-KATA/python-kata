04da5f6041423572273c700638c19a6b9e95562d
dwl21 <nggus5@gamil.com>
2023-10-29 10:34:43 +0000

    amend article keyword

--- dao/article_keyword_dao.py
+++ dao/article_keyword_dao.py
@@ -1,17 +1,17 @@
 from typing import Optional
-from article_keyword import article_keyword
+from article_keyword import ArticleKeyword
 
 article_keywords_db = {}
 
-def save(name: str) -> Optional[Article_keyword]:
+def save(name: str) -> Optional[ArticleKeyword]:
     if name not in article_keywords_db.values():
-        article_keyword = Article_keyword(id=len(article_keywords_db) + 1, name=name)
+        article_keyword = ArticleKeyword(id=len(article_keywords_db) + 1, name=name)
         article_keywords_db[article_keyword.id] = article_keyword
         return article_keyword
     return None
 
 
-def find(id: int) -> Optional[Article_keyword]:
+def find(id: int) -> Optional[ArticleKeyword]:
     if id in article_keywords_db:
         return article_keywords_db[id]
     return None
@@ -21,11 +21,11 @@ def find_all() -> list:
     return list(article_keywords_db.values())
 
 
-def edit(article_keyword: Article_keyword):
+def edit(article_keyword: ArticleKeyword):
     if article_keyword.id in article_keywords_db:
         article_keywords_db[id] = article_keyword
 
 
-def delete(article_keyword: Article_keyword):
+def delete(article_keyword: ArticleKeyword):
     if article_keyword.id in article_keywords_db:
         article_keywords_db.pop(article_keyword.id)
